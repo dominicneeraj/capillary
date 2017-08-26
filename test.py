67266a00f601bc4db5135984c4a16de21c973b5f
Neeraj giri <giri492neeraj@iitkgp.ac.in>
2017-08-26 18:55:58 +0530

    gdbg

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-df=pd.read_csv('training.csv')
+df=pd.read_csv('training1.csv')
 shuffled_ratings = df.sample(frac=1., random_state=1446557)
-shuffled_ratings.to_csv('training1.csv')
+shuffled_ratings.to_csv('training2.csv')
